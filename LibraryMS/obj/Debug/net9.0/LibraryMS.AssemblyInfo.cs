d4d7d077d15182ae7c4a2f3c72960313c559d54b
nur <fatmanur_agacbacak@hotmail.com>
2025-01-19 21:47:14 +0300

    Last Changes

--- LibraryMS/obj/Debug/net9.0/LibraryMS.AssemblyInfo.cs
+++ LibraryMS/obj/Debug/net9.0/LibraryMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b0bf4be825c10f1c1e7d073ab98517bead5dd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a998db976d1b300b7f92bb36d693390a5a0f5a9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
